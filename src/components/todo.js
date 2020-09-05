0fe4a41940db359e7d11f132b990172072ad6228
mohammadmahmood441@gmail.com <m.mahmood8176@gmail.com>
2020-09-05 20:15:48 +0500

    Mirage server client data sending

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,14 +5,26 @@ let baseUrl = "fakeapi";
 export const Todos = () => {
 
     useEffect(() => {
-        fetch(`/${baseUrl}/getTodos`)
+        fetch(`/${baseUrl}/getUsers`)
             .then(res => res.json())
             .then(data => console.log('data'.data))
     })
 
+    const addTodoHandler = () => {
+        fetch(`/${baseUrl}/addTodo`,{
+            method: "POST",
+            body: {id: 32, text: "text"}
+        }).then(res =>{
+            console.log("success", res)
+        }).catch(error =>{
+            console.log("error addtodo",error)
+        })
+    }
+
     return (
         <div>
             Hello From Todo
+            <button onClick= {addTodoHandler}>addTodoHandler</button>
         </div>
     )
 }
--- src/mirage/index.js
+++ src/mirage/index.js
@@ -2,10 +2,18 @@ import { createServer } from "miragejs"
 
 export default function () {
     let arr = [{id : "1", text : "hello"}]
+    let user = [{name: "mahmood", age: 26}]
   createServer({
     routes() {
         this.namespace = "/fakeapi"
         this.get("/getTodos", {todos:arr})
+
+        this.get("/getUsers", {todos:user})
+
+        this.post("/addTodo",(_,req)=>{
+          console.log('req',req);
+          arr.push(req.requestBody)
+        })
         // this.post()     // create or add data
         // this.put()      // update data
         // this.delete()   // for delete of data
